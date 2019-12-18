2446e249ee2e676a297245194aeb57dd8c84ebe2
ilsalareb1 <50841067+ilsalareb1@users.noreply.github.com>
2019-12-18 11:47:27 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const path = require("path");
+const hbs = require("hbs");
 const express = require("express");
 const app = express();
 
-
-
-app.get("/this is so cool", (req, res)=>{
-    res.send("Is this working?Lets see");
+//define paths for express
+const publicDirectoryPath = path.join(__dirname, "../public");
+const partialPath = path.join(__dirname, "../templates/partials");
+const viewsPath = path.join(__dirname, "../templates/views");
+//setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath); //telling express to get templates from templates/views folder
+hbs.registerPartials(partialPath);
+app.get(" ", async (req, res) => {
+try {
+res.render("index", {
+    title: "Our first Express App"
 });
-app.listen(3000, ()=> {
-    console.log("Listening on Port 3000");
+} catch {
+res.status(500).send();
+}
+});
+
+app.get("/about/:id", async (req, res) => {
+    const 
+    try {
+    res.render("index", {
+        title: `${name}`
+    });
+    } catch {
+    res.status(500).send();
+    }
+});
+
+app.get("/swag", async (req, res) => {
+    try {
+    res.render("swag");
+    } catch {
+    res.status(500).send();
+    }
 });
